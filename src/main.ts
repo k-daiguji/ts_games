509a93305b0b9a99d05b9a1e1088ef5d47a07c3e
k-daiguji <108970032+k-daiguji@users.noreply.github.com>
2025-08-09 00:05:44 +0900

    Add checks css (#12)

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,8 @@ const generateChild = (tagName: keyof HTMLElementTagNameMap) => {
   return document.body.appendChild(child);
 };
 const app = generateChild("div");
-app.className = "h-100";
 app.innerHTML = greet("World");
+const checks1 = generateChild("div");
+checks1.className = "checks h-20 w-20";
+const checks2 = generateChild("div");
+checks2.className = "checks2 h-20 w-20";
