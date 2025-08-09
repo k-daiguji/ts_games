ea4282243d2a0dd5cbdbf6b689cfc600cac62de4
k-daiguji <hikatteru20@gmail.com>
2025-08-10 02:19:50 +0900

    Fixed check tile

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { dom } from "./dom";
 const sizes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const elements = sizes.map(() => {
   const cells = sizes.map(() =>
-    dom.create("div").setClass("checks", "grid", "w-10"),
+    dom.create("div").setClass("checks", "grid", "tile"),
   );
-  return dom.create("div").setClass("d-flex", "h-10").setChildren(cells);
+  return dom.create("div").setClass("d-flex").setChildren(cells);
 });
 dom.setChildToElement("root", elements);
