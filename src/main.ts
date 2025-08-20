6fbde945c61e496a612f94a8bd922ceedd89f0da
k-daiguji <hikatteru20@gmail.com>
2025-08-20 20:51:47 +0900

    Add css custom property

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { dom } from "./dom";
 
 const map = [
-  [2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2],
-  [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1],
-  [1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1],
-  [1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1],
+  [2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2],
+  [1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1],
+  [1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1],
+  [1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1],
+  [1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1],
 ];
 const elements = map.map((row) => {
-  const cells = row.map((i) => dom.create("div").setClass(`tile${i}`, "tile"));
+  const cells = row.map((i) => dom.create("div").setClass(`color${i}`, "tile"));
   return dom.create("div").setClass("d-flex").setChildren(cells);
 });
 dom.setChildToElement("root", elements);
