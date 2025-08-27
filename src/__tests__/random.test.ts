21b69b8e4489aa9069380d442b93fbadafdf7198
k-daiguji <108970032+k-daiguji@users.noreply.github.com>
2025-08-27 21:29:11 +0900

    Add a random number generator (#30)

---
+++ src/__tests__/random.test.ts
@@ -0,0 +1,36 @@
+import { test, vi } from "vitest";
+import { random } from "../random";
+
+test.for([
+  [50, 0, 0],
+  [50, 0.01, 0],
+  [50, 0.02, 1],
+  [50, 0.97, 48],
+  [50, 0.98, 49],
+  [50, 0.99, 49],
+  [50, 1, 50],
+  [100, 0, 0],
+  [100, 0.009, 0],
+  [100, 0.01, 1],
+  [100, 0.989, 98],
+  [100, 0.99, 99],
+  [100, 0.999, 99],
+  [100, 1, 100],
+  [200, 0, 0],
+  [200, 0.004, 0],
+  [200, 0.005, 1],
+  [200, 0.994, 198],
+  [200, 0.995, 199],
+  [200, 0.999, 199],
+  [200, 1, 200],
+])(
+  "When the maximum value is %s and the random number is %s, the result is %s.",
+  ([max, randomNumber, expected], { expect }) => {
+    const generate = random(max);
+    vi.spyOn(Math, "random").mockReturnValue(randomNumber);
+
+    const actual = generate();
+
+    expect(actual).toBe(expected);
+  },
+);
