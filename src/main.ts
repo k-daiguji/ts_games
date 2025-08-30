61019b761673b2e70ddb48d83e97b65e6adcd757
k-daiguji <hikatteru20@gmail.com>
2025-08-31 03:19:06 +0900

    Move cats

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,25 @@
+import "./extensions/array";
+import { random } from "./random";
+import { loop } from "./timer";
+
 const stage = document.getElementById("stage");
 const templateCat = document.getElementById("cat");
 if (stage && templateCat) {
   const cloneCat = templateCat.cloneNode(true);
   document.body.removeChild(templateCat);
-  [
-    ["0", "40%"],
-    ["40%", "80%"],
-    ["40%", "0"],
-    ["80%", "40%"],
-  ].forEach(([top, left]) => {
-    const cat = cloneCat.cloneNode(true) as HTMLElement;
-    cat.style.top = top;
-    cat.style.left = left;
+  const cats = Array.range(3).map(
+    () => cloneCat.cloneNode(true) as HTMLElement,
+  );
+  cats.forEach((cat) => {
     stage.appendChild(cat);
   });
+  const generate = random(90);
+  loop(
+    () =>
+      cats.forEach((cat) => {
+        cat.style.top = `${generate()}%`;
+        cat.style.left = `${generate()}%`;
+      }),
+    5,
+  );
 }
