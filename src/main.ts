54d6a95cf9f11ec638219a401b6ae851429f58aa
k-daiguji <hikatteru20@gmail.com>
2025-08-31 02:12:31 +0900

    Change element id

--- src/main.ts
+++ src/main.ts
@@ -11,4 +11,4 @@ const elements = map.map((row) => {
   const cells = row.map((i) => dom.create("div").setClass(`color${i}`, "tile"));
   return dom.create("div").setClass("d-flex").setChildren(cells);
 });
-dom.setChildToElement("root", elements);
+dom.setChildToElement("stage", elements);
