6d73398134516cb65effa5cb44cb7b14d6c9e45b
k-daiguji <108970032+k-daiguji@users.noreply.github.com>
2025-08-31 02:13:21 +0900

    Change element id (#36)

--- src/__tests__/dom.test.ts
+++ src/__tests__/dom.test.ts
@@ -3,9 +3,9 @@ import { test } from "vitest";
 import { dom } from "../dom";
 
 test("Added custom element for body.", ({ expect }) => {
-  const root = document.createElement("div");
-  root.id = "root";
-  document.body.appendChild(root);
+  const stage = document.createElement("div");
+  stage.id = "stage";
+  document.body.appendChild(stage);
   const indexes = [1, 2];
 
   indexes.forEach((i) => {
@@ -18,10 +18,10 @@ test("Added custom element for body.", ({ expect }) => {
         .setText(String(index));
     });
     const row = dom.create("div").setId(`row-${i}`).setChildren(cells);
-    dom.setChildToElement("root", [row]);
+    dom.setChildToElement("stage", [row]);
   });
 
   expect(String(document.body)).toBe(
-    `<body><div id="root"><div id="row-1"><div id="cell-1" class="test-class">1</div><div id="cell-2" class="test-class">2</div></div><div id="row-2"><div id="cell-2" class="test-class">2</div><div id="cell-4" class="test-class">4</div></div></div></body>`,
+    `<body><div id="stage"><div id="row-1"><div id="cell-1" class="test-class">1</div><div id="cell-2" class="test-class">2</div></div><div id="row-2"><div id="cell-2" class="test-class">2</div><div id="cell-4" class="test-class">4</div></div></div></body>`,
   );
 });
