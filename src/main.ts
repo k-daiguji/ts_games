19a783cef094c9d03704f4cfdf88223882ece501
k-daiguji <108970032+k-daiguji@users.noreply.github.com>
2025-08-31 13:14:52 +0900

    Timer speed can change (#50)

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import "./extensions/array";
 import { random } from "./random";
 import { timer } from "./timer";
 
+const speed = document.getElementById("speed");
 const date = document.getElementById("date");
 const stage = document.getElementById("stage");
 const templateCat = document.getElementById("cat");
-if (date && stage && templateCat) {
+if (speed && date && stage && templateCat) {
   const cloneCat = templateCat.cloneNode(true);
   document.body.removeChild(templateCat);
   const cats = Array.range(3).map(
@@ -18,7 +19,7 @@ if (date && stage && templateCat) {
   };
   let dateCount = 0;
   date.innerText = dateCount.toString();
-  const delay = 1;
+  let delay = 1;
   const start = () =>
     timer(() => {
       date.innerText = (++dateCount).toString();
@@ -33,4 +34,9 @@ if (date && stage && templateCat) {
     };
     stage.appendChild(cat);
   });
+  speed.oninput = (e) => {
+    stop();
+    delay = 1 / Number((e.target as HTMLInputElement).value);
+    stop = start();
+  };
 }
