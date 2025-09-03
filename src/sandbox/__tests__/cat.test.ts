e76027adc523b2da832bd8971afc412d5e77f86c
k-daiguji <108970032+k-daiguji@users.noreply.github.com>
2025-09-03 22:27:38 +0900

    Fixed cat (#64)

--- src/sandbox/__tests__/cat.test.ts
+++ src/sandbox/__tests__/cat.test.ts
@@ -1,26 +1,12 @@
 import { test } from "vitest";
-import { Cat } from "../cat";
 
-test.for<number>(Array.range(47))(
-  "When the random number is %s, the sex is female.",
-  (randomNumber, { expect }) => {
-    const dummyRandom = () => () => randomNumber;
-    const cat = Cat.create(dummyRandom);
+import { createCat } from "../cat";
 
-    const actual = cat.status;
+test.for<[string, boolean]>([
+  ["female", false],
+  ["male", true],
+])("Created %s cat.", ([, sex], { expect }) => {
+  const cat = createCat(sex);
 
-    expect(actual).toStrictEqual({ sex: true });
-  },
-);
-
-test.for<number>(Array.range(100, { start: 48 }))(
-  "When the random number is %s, the sex is male.",
-  (randomNumber, { expect }) => {
-    const dummyRandom = () => () => randomNumber;
-    const cat = Cat.create(dummyRandom);
-
-    const actual = cat.status;
-
-    expect(actual).toStrictEqual({ sex: false });
-  },
-);
+  expect(cat.sex).toBe(sex);
+});
