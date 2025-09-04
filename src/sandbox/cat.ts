e213d753164b0dc93c58de6780ae757c79e42f03
k-daiguji <hikatteru20@gmail.com>
2025-09-04 20:57:06 +0900

    Add cat's age

--- src/sandbox/cat.ts
+++ src/sandbox/cat.ts
@@ -1,1 +1,7 @@
-export const createCat = (sex: boolean) => ({ sex });
+import type { Age } from "../age";
+
+export const createCat = (sex: boolean, age: Age) => ({
+  age: age.toText,
+  pass: age.increment,
+  sex,
+});
