a55885f31b518a9978ce1150d67534d350cf9d8b
k-daiguji <108970032+k-daiguji@users.noreply.github.com>
2025-09-04 23:38:12 +0900

    Fixed timer (#70)

--- src/__tests__/timer.test.ts
+++ src/__tests__/timer.test.ts
@@ -1,74 +1,90 @@
 import { describe, test, vi } from "vitest";
-import { timer } from "../timer";
+import { start } from "../timer";
 
-describe("loop", () => {
-  test.for<[number, number]>([
-    [9_999, 9],
-    [10_000, 10],
-    [10_999, 10],
-    [11_000, 11],
-  ])(
-    "After %s milliseconds, the counter becomes %s.",
-    ([ms, expected], { expect }) => {
-      vi.useFakeTimers();
-      let counter = 0;
-      timer(() => counter++, 1);
+test.for<[number, number]>([
+  [9_999, 9],
+  [10_000, 10],
+  [10_999, 10],
+  [11_000, 11],
+])(
+  "After %s milliseconds, the counter becomes %s.",
+  ([ms, expected], { expect }) => {
+    vi.useFakeTimers();
+    let counter = 0;
+    start(() => counter++, 1);
 
-      vi.advanceTimersByTime(ms);
+    vi.advanceTimersByTime(ms);
 
-      expect(counter).toBe(expected);
-    },
-  );
+    expect(counter).toBe(expected);
+  },
+);
 
-  test("When the timer is stopped, the counter is not incremented.", ({
+describe("Pause & Resume", () => {
+  test("When the timer is paused, the counter is not incremented.", ({
     expect,
   }) => {
     vi.useFakeTimers();
     let counter = 0;
-    const stop = timer(() => counter++, 1);
-    vi.advanceTimersByTime(10_000);
-
-    stop();
+    const { pause } = start(() => counter++, 1);
     vi.advanceTimersByTime(1_000);
 
-    expect(counter).toBe(10);
+    pause();
+    vi.advanceTimersByTime(2_000);
+
+    expect(counter).toBe(1);
   });
 
-  describe("Multiple", () => {
-    test("When the timer1 is stopped, the counter1 is not incremented.", ({
-      expect,
-    }) => {
-      vi.useFakeTimers();
-      let counter1 = 0;
-      const stop1 = timer(() => counter1++, 1);
-      let counter2 = 0;
-      timer(() => counter2++, 2);
-      vi.advanceTimersByTime(2_000);
+  test("When the timer is paused and then resumed, the counter will be incremented periodically.", ({
+    expect,
+  }) => {
+    vi.useFakeTimers();
+    let counter = 0;
+    const { pause } = start(() => counter++, 1);
+    vi.advanceTimersByTime(1_000);
+    const resume = pause();
+    vi.advanceTimersByTime(2_000);
 
-      stop1();
+    resume();
+    vi.advanceTimersByTime(4_000);
 
-      vi.advanceTimersByTime(2_000);
+    expect(counter).toBe(5);
+  });
+});
 
-      expect(counter1).toBe(2);
-      expect(counter2).toBe(2);
-    });
+describe("Multiple timer", () => {
+  test("When the timer1 is cleared, the counter1 is not incremented.", ({
+    expect,
+  }) => {
+    vi.useFakeTimers();
+    let counter1 = 0;
+    const timer1 = start(() => counter1++, 1);
+    let counter2 = 0;
+    start(() => counter2++, 2);
+    vi.advanceTimersByTime(2_000);
 
-    test("When the timer2 is stopped, the counter2 is not incremented.", ({
-      expect,
-    }) => {
-      vi.useFakeTimers();
-      let counter1 = 0;
-      timer(() => counter1++, 1);
-      let counter2 = 0;
-      const stop2 = timer(() => counter2++, 2);
-      vi.advanceTimersByTime(2_000);
+    timer1.clear();
 
-      stop2();
+    vi.advanceTimersByTime(2_000);
 
-      vi.advanceTimersByTime(2_000);
+    expect(counter1).toBe(2);
+    expect(counter2).toBe(2);
+  });
 
-      expect(counter1).toBe(4);
-      expect(counter2).toBe(1);
-    });
+  test("When the timer2 is cleared, the counter2 is not incremented.", ({
+    expect,
+  }) => {
+    vi.useFakeTimers();
+    let counter1 = 0;
+    start(() => counter1++, 1);
+    let counter2 = 0;
+    const timer2 = start(() => counter2++, 2);
+    vi.advanceTimersByTime(2_000);
+
+    timer2.clear();
+
+    vi.advanceTimersByTime(2_000);
+
+    expect(counter1).toBe(4);
+    expect(counter2).toBe(1);
   });
 });
--- src/timer.ts
+++ src/timer.ts
@@ -1,4 +1,23 @@
-export function timer(func: () => void, sec: number) {
-  const timeout = setInterval(func, sec * 1000);
-  return () => clearInterval(timeout);
+export type Resume = () => Timer;
+
+interface Timer {
+  clear: () => void;
+  pause: () => Resume;
+  shift: (sec: number) => Timer;
 }
+
+export const start = (func: () => void, sec: number): Timer => {
+  const timeout = setInterval(func, sec * 1000);
+  const clear = () => clearInterval(timeout);
+  return {
+    clear,
+    pause: () => {
+      clear();
+      return () => start(func, sec);
+    },
+    shift: (sec: number) => {
+      clear();
+      return start(func, sec);
+    },
+  };
+};
