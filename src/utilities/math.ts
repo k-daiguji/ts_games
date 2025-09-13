4e97b71168a46f74900f41b577a4de4fe5a5ba8f
k-daiguji <hikatteru20@gmail.com>
2025-09-13 16:35:57 +0900

    Convert to percentage

--- src/utilities/math.ts
+++ src/utilities/math.ts
@@ -2,3 +2,6 @@ export const roundUp = (value: number, devisor: number) => ({
   quotient: Math.floor(value / devisor),
   remainder: value % devisor,
 });
+
+export const clamp = (value: number, max: number, min: number) =>
+  Math.min(Math.max(value, min), max);
