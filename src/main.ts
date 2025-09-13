c80b1d5a1cfceffba3d066dc5e7192082a725a5b
k-daiguji <hikatteru20@gmail.com>
2025-09-13 13:54:38 +0900

    Add hover events

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import type { Age } from "./sandbox/age";
 import { toAge } from "./sandbox/age";
 import type { Sex } from "./sandbox/sex";
-import { initialize } from "./sandbox/sex";
+import { classifySex, initialize } from "./sandbox/sex";
 import { range } from "./utilities/array";
 import { cache, find, overwrite } from "./utilities/dom";
 import { generate } from "./utilities/random";
@@ -23,17 +23,23 @@ const judge = (a: Position, b: Position) =>
   isContact(a.top, b.top, 5) && isContact(a.left, b.left, 5);
 const month = find("#month") as Element;
 const cloneCat = cache("#cat");
-const createCat = (sex: Sex) => ({
-  age: toAge(),
-  element: cloneCat(),
-  sex,
-  coolTime: 12,
-  top: 0,
-  left: 0,
-});
+let isNext = true;
+const createCat = (sex: Sex) => {
+  const element = cloneCat();
+  element.onmouseover = () => {
+    isNext = false;
+  };
+  element.onmouseleave = () => {
+    isNext = true;
+  };
+  return { age: toAge(), element, sex, coolTime: 12, top: 0, left: 0 };
+};
 const adultAge = toAge(1);
-const lifespan = toAge(15);
-const main = (cats: Cat[]) => {
+const lifespan = toAge(10);
+const main = (cats: Cat[]) => (isNext ? update(cats) : next(cats));
+const speed = find("#speed") as HTMLInputElement;
+const next = (cats: Cat[]) => start(() => main(cats), 5 / Number(speed.value));
+const update = (cats: Cat[]) => {
   cats.forEach((cat) => {
     cat.age = cat.age.increment();
     if (adultAge.equal(cat.age)) cat.element.className = "adult";
@@ -44,17 +50,7 @@ const main = (cats: Cat[]) => {
     cat.element.style.left = `${cat.left}%`;
   });
   const aliveCats = cats.filter((cat) => !lifespan.equal(cat.age));
-  const { females, males } = aliveCats
-    .filter((cat) => !cat.coolTime)
-    .reduce<{ females: Cat[]; males: Cat[] }>(
-      (acc, cat) => {
-        const { females, males } = acc;
-        return cat.sex === "XX"
-          ? { females, males: [...males, cat] }
-          : { females: [...females, cat], males };
-      },
-      { females: [], males: [] },
-    );
+  const { females, males } = classifySex(aliveCats);
   const newCats = aliveCats.concat(
     males
       .flatMap((male) => {
@@ -74,9 +70,6 @@ const main = (cats: Cat[]) => {
     newCats.map((cat) => cat.element),
   );
   month.innerHTML = String(Number(month.innerHTML) + 1);
-  start(
-    () => main(newCats),
-    1 / Number((find("#speed") as HTMLInputElement).value),
-  );
+  next(newCats);
 };
 main([true, true, false, false].map((value) => createCat(initialize(value))));
