1c96419494f7c346799a54d96ac8af1d2e523649
k-daiguji <108970032+k-daiguji@users.noreply.github.com>
2025-09-13 09:34:27 +0900

    Add child cat (#90)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import type { Age } from "./sandbox/age";
+import { toAge } from "./sandbox/age";
 import { range } from "./utilities/array";
 import { cache, find, overwrite } from "./utilities/dom";
 import { generate } from "./utilities/random";
@@ -5,17 +7,22 @@ import { start } from "./utilities/timer";
 
 const month = find("#month") as Element;
 const cloneCat = cache("#cat");
-const createCat = () => cloneCat();
-const main = (cats: (HTMLElement | undefined)[]) => {
-  const aliveCats = cats.filter((cat) => !!cat);
-  aliveCats.forEach((cat) => {
-    cat.style.top = `${generate(100)}%`;
-    cat.style.left = `${generate(100)}%`;
+const createCat = () => ({ age: toAge(), element: cloneCat() });
+const adultAge = toAge(1);
+const main = (cats: { age: Age; element: HTMLElement }[]) => {
+  cats.forEach((cat) => {
+    cat.age = cat.age.increment();
+    if (adultAge.equal(cat.age)) cat.element.className = "adult";
+    cat.element.style.top = `${generate(100)}%`;
+    cat.element.style.left = `${generate(100)}%`;
   });
-  overwrite("#eden", aliveCats);
+  overwrite(
+    "#eden",
+    cats.map((cat) => cat.element),
+  );
   month.innerHTML = String(Number(month.innerHTML) + 1);
   start(
-    () => main([...aliveCats, createCat()]),
+    () => main([...cats, createCat()]),
     1 / Number((find("#speed") as HTMLInputElement).value),
   );
 };
