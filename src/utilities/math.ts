25b2b8cc8fa649ebdb1ae8c51af1568cf2f74c8b
k-daiguji <hikatteru20@gmail.com>
2025-09-15 11:00:20 +0900

    Merge math files

--- src/utilities/math.ts
+++ src/utilities/math.ts
@@ -1,7 +1,9 @@
+export const clamp = (value: number, max: number, min: number) =>
+  Math.min(Math.max(value, min), max);
+
+export const generate = (max: number) => Math.floor(Math.random() * max);
+
 export const roundUp = (value: number, devisor: number) => ({
   quotient: Math.floor(value / devisor),
   remainder: value % devisor,
 });
-
-export const clamp = (value: number, max: number, min: number) =>
-  Math.min(Math.max(value, min), max);
